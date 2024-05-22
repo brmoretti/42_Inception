2f0a250f6eabe628ad8e924621e0582772338de2
Bruno Moretti <brmoretti@gmail.com>
2024-05-22 17:55:01 -0300

    05/22 end of day

--- srcs/requirements/mariadb/conf/init.sql
+++ srcs/requirements/mariadb/conf/init.sql
@@ -3,4 +3,3 @@ USE wordpress;
 CREATE USER IF NOT EXISTS 'wordpress'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost';
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
