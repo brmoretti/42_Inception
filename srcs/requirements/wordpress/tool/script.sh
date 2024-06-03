53bd95003780b1369d83eb46d95f19b5c1f524a3
nivicius <niviciusdev@gmail.com>
2024-06-02 21:17:27 -0300

    modified script of wordpress to test if redis is working

--- srcs/requirements/wordpress/tool/script.sh
+++ srcs/requirements/wordpress/tool/script.sh
@@ -9,7 +9,7 @@ wp --allow-root config create \
 
 wp core install --allow-root \
 	--path=/var/www/wordpress \
-	--title="Será queo largatu mama?" \
+	--title="Incept time" \
 	--url=bmoretti.42.fr \
 	--admin_user=$WORDPRESS_USER \
 	--admin_password=$WORDPRESS_PASSWORD \
@@ -17,5 +17,16 @@ wp core install --allow-root \
 
 # Activate the Twenty Twenty-Two theme.
 wp --allow-root theme activate twentytwentytwo
+
+# Bonus part redis
+wp plugin install redis-cache --activate --allow-root --path=/var/www/wordpress/
+wp redis enable --allow-root --path=/var/www/wordpress/
+
+# configure redis cache
+wp config set WP_REDIS_PORT 6379 --add --type=constant --allow-root --path=/var/www/wordpress/
+wp config set WP_REDIS_HOST redis --add --type=constant --allow-root --path=/var/www/wordpress/
+
+# configure wordpress
+wp config set WP_CACHE true --add --type=constant --allow-root --path/var/www/wordpress/
 
 exec php-fpm7.4 -F
